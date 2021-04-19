8a98368c59e22efda03113e4be87afe4ed4a901c
Casey Johnson <johnsoncm3@gmail.com>
2021-04-19 00:41:33 -0400

    prompt functionality is working, creates a readme, and writes answers to it

--- Develop/index.js
+++ Develop/index.js
@@ -5,15 +5,17 @@ const util = require('util');
 
 
 
-
-
 // TODO: Create an array of questions for user input
 // const questions = [];
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const promptUser = () => {
-    return inquirer.prompt([
+// const promptUser = () => {
+//     return 
+
+// inquirer.prompt([
+
+const questions = [
         {
         type: 'input',
         name: 'title',
@@ -62,25 +64,27 @@ const promptUser = () => {
         name: 'email',
         message: "What is your email address?",
         },
-]);
-};
+];
+
+inquirer 
+    .prompt(questions)
+    .then((data) => {
+
+    fs.writeFile('readme.md', buildReadMe(data.title , data.description, data.install, data.usage, data.contribution, 
+       data.test, data.license, data.github, data.email), (err) =>
+       err ? console.log(err) :
+       console.log(data)
+    )
+
+});
 
 function buildReadMe(title, description, install, usage, contribution, test, license, github, email){
     return `${title}<br>Provide a short description of your project:${description}<br>Installation:What are the steps required to install your project?${install}<br>
     Usage: Provide instructions and examples for use.${usage}<br>Credits: List your collaborators, if any.${contribution}<br>
     Test Instructions:${test}<br>License:What is the license for your project?${license}<br>Questions: Github Profile:${github}<br>
     Email${email}`
-};
+}; 
 
-    .then((data) => {
-
-     fs.writeFile('readme.md', buildReadMe(data.title , data.description, data.install, data.usage, data.contribution, 
-        data.test, data.license, data.github, data.email), (err) =>
-        err ? console.log(err) :
-        console.log(data)
-     )
-
-});
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {
---
+++ Develop/readme.md
@@ -0,0 +1,4 @@
+readme generator<br>Provide a short description of your project:this generates a read me file<br>Installation:What are the steps required to install your project?use command lines<br>
+    Usage: Provide instructions and examples for use.available for all<br>Credits: List your collaborators, if any.be careful with my code<br>
+    Test Instructions:default<br>License:What is the license for your project?undefined<br>Questions: Github Profile:johnsoncm<br>
+    Emailjohnsoncm3@gmail.com
